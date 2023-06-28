1d78a8279b700d85c7f84ca2a2cdaf38fa20b267
Andrei Arkhipov <it.arkhipov@gmail.com>
2023-06-28 08:47:48 +0300

    get status code check

--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
-import pytest
+import requests
 
 class TestMath:
 
@@ -6,3 +6,11 @@ class TestMath:
     def test_check_number():
         a = 5
         assert a == 5
+
+    @staticmethod
+    def test_get_response_code():
+        url = "https://jsonplaceholder.typicode.com/posts"
+
+        response = requests.get(url)
+
+        assert response.status_code == 200
