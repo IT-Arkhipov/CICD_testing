5dd116bbb7c20d5452c8d08c28ce2e252fa391ce
IT-Arkhipov <it.arkhipov@gmail.com>
2023-11-04 17:09:01 +0300

    allure config

--- test_main.py
+++ test_main.py
@@ -1,13 +1,16 @@
 import requests
+import allure
+
 
 class TestMath:
-
     @staticmethod
     def test_check_number():
-        a = 5
-        assert a == 5
+        with allure.step('assert a == 5'):
+            a = 5
+            assert a == 5
 
     @staticmethod
+    @allure.step('Test response code')
     def test_get_response_code():
         url = "https://jsonplaceholder.typicode.com/posts"
 
